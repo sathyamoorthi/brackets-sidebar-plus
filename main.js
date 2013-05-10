1391d338957ea63d25d38d2066a2abe94c64fba2
sathyamoorthi <sathyamoorthi10@gmail.com>
2013-05-10 15:50:31 +0530

    right-click, triangle selection problems solved.
    Below issues are fixed.
    
    1.Right-click has an issue while sidebar is in Auto/Show hide mode.
    
    2.a.Choose View->Hide Side Bar, b.Select a file under Working Files,
    c.Do auto hide/show,
    d.Then again choose View->Show Side Bar. In this case you will not see
    rectangle pointer. You have to again click on that file to see it.

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets, window */
 
+//To know more about this extension, use "Show Developer Tools". Understanding the role of .horz-resizer, .content, #sidebar are mandatory.
 define(function (require, exports, module) {
-    "use strict";    
-    
-$(".horz-resizer").on("mouseover", function () {
-    
-    var sidebar = $("#sidebar"),
-        hrz = $(".horz-resizer"),
-        content = $(".content");
+    "use strict";     
+
+$(".horz-resizer").on("mouseenter", OpenSidebar);
+$(".content").on("mouseenter", CollapseSidebar);
+$(document).on("mouseleave", CollapseSidebar);
+$("#sidebar").on("panelExpanded", ShowSlectionTriangle);    
+
+function OpenSidebar() 
+{    
+    var sidebar = $("#sidebar"), hrz = $(".horz-resizer"), content = $(".content");
 
     if (hrz.parent().hasClass("sidebar") == false && content.is(':animated') == false) 
     {
-        sidebar.css("display", "-webkit-box");
-        sidebar.attr("data-mover-show", true); 
+        sidebar.css("display", "-webkit-box");         
         hrz.css("display", "none");
-        content.animate({left: ((sidebar.width() > 0) ? sidebar.width() : 200)}, 300, function()
+        
+        content.animate({left: ((sidebar.width() > 0) ? sidebar.width() : 200)}, 250, function()
                         {
-                                $(".sidebar-selection-triangle").css("display", "block");
+                                sidebar.attr("data-mover-show", true);
+                                ShowSlectionTriangle();
                         });
     }
-});
-
-$("#sidebar").on("mouseleave", function () {
+}
     
-    var sidebar = $("#sidebar"),
-        hrz = $(".horz-resizer"),
-        content = $(".content");
+function CollapseSidebar()
+{
+    var sidebar = $("#sidebar"), hrz = $(".horz-resizer"), content = $(".content");
 
     if (sidebar.attr("data-mover-show") == "true") 
     {        
         hrz.css("display", "block");          
         $(".sidebar-selection-triangle").css("display", "none");
+        $("#project-files-container").find(".scroller-shadow").css("display", "none");
         content.stop();
         
-        content.animate({left: 0}, 300, function() {            
+        content.animate({left: 0}, 250, function() {            
             sidebar.removeAttr("data-mover-show", true);
             sidebar.css("display", "none");
         });
     }
-});
+}
+    
+    
+function ShowSlectionTriangle()
+{
+    $("#project-files-container").find(".scroller-shadow").css("display", "block");
+    $(".sidebar-selection").filter(function()
+                                    {
+                                        return $(this).css("display") == "block";
+                                    }).parent().find(".sidebar-selection-triangle").css("display", "block");
+}
+    
 });
