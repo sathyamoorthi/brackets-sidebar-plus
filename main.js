834ebd6d6c436961be4b09eea0fb5ff6713da8a7
sathyamoorthi <sathyamoorthi10@gmail.com>
2013-10-23 17:57:19 +0530

    Timer added to collapse sidebar
    see this discussion.
    https://groups.google.com/forum/#!topic/brackets-dev/yML-dxwQH7Q

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window, document */
+/*global define, $, brackets, window, document, clearTimeout, setTimeout */
 
 //To know more about this extension, use "Show Developer Tools". Understanding the role of .horz-resizer, .content, #sidebar are mandatory.
 define(function (require, exports, module) {
     "use strict";
 
+    var collapseDelay = 800,
+        collapseTimer = null;
+
     $.extend($.easing, {
         def: 'easeOutCubic',
         easeOutCubic: function (x, t, b, c, d) {
             return c * ((t = t / d - 1) * t * t + 1) + b;
+        },
+        easeInOutExpo: function (x, t, b, c, d) {
+            if (t==0) return b;
+            if (t==d) return b+c;
+            if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
         }
     });
 
@@ -30,10 +39,20 @@ define(function (require, exports, module) {
         }).parent().find(".sidebar-selection-triangle").css("display", "block");
     }
 
+    function killCollapseTimer() {
+        clearTimeout(collapseTimer);
+        collapseTimer = null;
+    }
+
     function openSidebar() {
         var sidebar = $("#sidebar"),
             hrz = $(".horz-resizer"),
             content = $(".content");
+
+        if (collapseTimer) {
+            killCollapseTimer();
+            return;
+        }
 
         if (hrz.parent().hasClass("sidebar") === false && content.is(':animated') === false) {
             sidebar.css("display", "-webkit-box");
@@ -53,19 +72,28 @@ define(function (require, exports, module) {
             hrz = $(".horz-resizer"),
             content = $(".content");
 
-        if (sidebar.attr("data-hover-show") === "true") {
-            hrz.css("display", "block");
-            panelCollapsed();
-            content.stop();
-
-            content.animate({
-                left: 0
-            }, 350, "easeOutCubic", function () {
-                sidebar.removeAttr("data-hover-show", true);
-                sidebar.css("display", "none");
-            });
+        if (collapseTimer) {
+            return;
         }
+
+        collapseTimer = setTimeout(function () {
+            if (sidebar.attr("data-hover-show") === "true") {
+                hrz.css("display", "block");
+                panelCollapsed();
+                content.stop();
+
+                content.animate({
+                    left: 0
+                }, 1000, "easeInOutExpo", function () {
+                    sidebar.removeAttr("data-hover-show", true);
+                    sidebar.css("display", "none");
+                });
+            }
+
+            collapseTimer = null;
+        }, collapseDelay);
     }
+
 
     $(document).mousemove(function (e) {
         if (e.pageX <= 35 && !$("#sidebar").attr("data-hover-show")) {
@@ -75,6 +103,7 @@ define(function (require, exports, module) {
 
     $(".content").on("mouseenter", collapseSidebar);
     $(document).on("mouseleave", collapseSidebar);
+    $("#sidebar").on("mousemove", killCollapseTimer);
     $("#sidebar").on("panelExpanded", panelExpanded);
     $("#sidebar").on("panelCollapsed", panelCollapsed);
     $(".content").css("background-color", "#ffffff");
